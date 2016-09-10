d7743b257ff7ce6a1cefc3d793f7b8f2fa9a57a7
Alex MCFerron <alexmac2017@gmail.com>
2016-09-09 17:46:02 -0700

    'i'

---
+++ HelloSignConsoleTester/HelloSignConsoleTester/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloSignConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("HelloWorld HelloSign");
+
+            Console.ReadKey();
+
+        }
+    }
+}
