7df6071be5b0758235200715524fa546fbcc713a
Alex MCFerron <alexmac2017@gmail.com>
2016-09-09 18:52:55 -0700

    fixed untracked files

--- HelloSignConsoleTester/HelloSignConsoleTester/Program.cs
+++ HelloSignConsoleTester/HelloSignConsoleTester/Program.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Specialized;
+
 
 namespace HelloSignConsoleTester
 {
     class Program
     {
+        //https://support.microsoft.com/en-us/kb/815786
+
         static void Main(string[] args)
         {
 
             Console.WriteLine("HelloWorld HelloSign");
 
+            var sAttr = ConfigurationManager.AppSettings.Get("Key0");
+            Console.WriteLine(sAttr + " Is key");
+
+
+
             Console.ReadKey();
+
+        }
+
+        static void GetConnectionStrings()
+        {
+    
 
         }
     }
